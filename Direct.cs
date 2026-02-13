68f14da059dcfb62d89a9e317f7209b9d376eda3
Alina <alina-lapkina@mail.ru>
2026-02-13 18:04:34 +0300

    Добавьте файлы проекта.

---
+++ Direct.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Morza
+{
+    internal static class Direct
+    {
+        //я хотела потренироваться со статическими классами + мне кажется тут нужен чисто функционал именно в разрезе морзы,
+        //она статичная и не меняется
+
+        static string[] Morz ={"·—","—···","·——","——·","—··",
+            "·","···—","——··","··","·———",
+            "—·—","·—··","——","—·","———",
+            "·——·","·—·","···","—","··—",
+            "··—·","····","—·—·","———·","————",
+            "——·—","——·——","—·——","—··—","··—··","··——","·—·—"};
+
+        static string[] words = { "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М",
+            "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
+
+        //Вернет соответствующую букве символьное значение
+        static public string Code(char A)
+        {
+            string contr = A.ToString();
+            contr = contr.ToUpper();
+            int size = Morz.Length - 1;
+            //Проверка с выбросом исключения, если был передана не буква
+            Check(contr);
+
+
+            int j = Array.IndexOf(words, contr.ToString());
+            //Возвращаем из массива морза нужное значение
+            return Morz[Array.IndexOf(words, contr.ToString())];
+
+
+        }
+
+        //Перегрузка, если будет вводиться строка и целое слово
+        static public string Code(string contr)
+        {
+            //Проверка с выбросом исключения, если был переданы не буквы
+            Check(contr);
+            //Если строка всего 1 буква перенаправим на другую функию
+            if (contr.Length == 1)
+            {
+                return Code(Char.Parse(contr)).ToString();
+            }
+            else
+            {
+                contr = contr.ToUpper();
+                string buf = "";
+                int size = contr.Length;
+                int j = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    //Если это предложение и там будет какой-то пробел или точка, то увеличиваем счетчик и пропускаем эту иттерацию
+                    if (!Char.IsLetter(contr[i]))
+                    {
+                        buf += contr[i].ToString() + " ";
+                        continue;
+                    }
+                    else
+                    {
+                        //Находим где в массиве слов наша буква и на каком индексе она находится
+                        j = Array.IndexOf(words, contr[i].ToString());
+                        buf += Morz[j] + " ";
+
+                    }
+
+                }
+                return buf;
+            }
+
+        }
+        static public string UnCode(char A)
+        {
+
+            string contr = A.ToString();
+            contr = contr.ToUpper();
+            int size = Morz.Length - 1;
+            //Проверка с выбросом исключения, если был переданы не буквы
+            Check(contr, false);
+
+            //Если введенный символ не является буквой
+            if (!Char.IsLetter(Char.Parse(contr)))
+            {
+                throw new Exception("Значение не буква");
+            }
+            else
+            {
+                //Возвращаем из массива морза нужное значение
+                return words[Array.IndexOf(Morz, contr.ToString())];
+            }
+
+        }
+        static public string UnCode(string contr)
+        {
+            //Проверка с выбросом исключения, если был переданы не буквы
+            Check(contr, false);
+            string buf = "";
+            //Если строка всего 1 буква перенаправим на другую функию
+            if (contr.Length == 1)
+            {
+                return Code(Char.Parse(contr)).ToString();
+            }
+            else
+            {
+
+                int j = 0;
+                contr = contr.Replace((char)45, (char)8212);
+                string[] split = contr.Split(' ');
+                int size = split.Length;
+
+
+                for (int i = 0; i < size; i++)
+                {
+
+                    if (split[i] == "")
+                    {
+                        buf += " ";
+                        continue;
+                    }
+
+                    j = Array.IndexOf(Morz, split[i]);
+                    buf += words[j];
+
+                }
+                buf = buf.ToLower();
+                Format(ref buf);
+                return buf;
+            }
+
+        }
+
+        static void Format(ref string text1)
+        {
+            char spl = '.';
+            StringBuilder textB = new StringBuilder(text1);
+            int i = 0;
+            while (i >= 0)
+            {
+                while (!Char.IsLetter(textB[i])) i++;
+                textB[i] = Char.ToUpper(textB[i]);
+                i = text1.IndexOf(spl, i);
+            }
+            text1 = textB.ToString();
+        }
+
+        //Метод исключений
+        static void Check(string contr, bool letter = true)
+        {
+            if (letter)
+            {
+                contr=contr.ToUpper();
+                foreach (char ch in contr)
+                {
+                    int i = Array.IndexOf(words,ch.ToString());
+                    if(i<0) throw new Exception("Программа не поддерживает данную раскладку");
+
+                }
+
+                foreach (char ch in contr)
+                {
+                    if (!Char.IsLetter(ch) && ch != ' ') throw new Exception("Веденные символы не являются буквами");
+                }
+            }
+            else
+            {
+                foreach (char ch in contr)
+                {
+                    if (ch != ' ' && ch != 45 && ch != 183) throw new Exception("Введенные символы не шифр");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Diagnostics;
+
+namespace Morza
+{
+
+    class Programm
+    {
+        /*#1 (обязательно)
+        Создайте приложение для перевода обычного текста
+        в азбуку Морзе.Пользователь вводит текст.Приложение
+        отображает введенный текст азбукой Морзе.Используйте
+        механизмы пространств имён.*/
+
+        static void Main()
+        {
+            while (true)
+            {
+                string text ="";
+                int cod = 0;
+                Console.WriteLine("Введите текст для шифрования/расшифрования");
+                text = Console.ReadLine()!;
+          
+                Console.Write(@"Зашифровать или расшифровать введенный текст?
+1-Зашифровать
+0-Расшифровать
+3-Закончить
+Ввод: ");
+                cod = Convert.ToInt32(Console.ReadLine());
+                if (cod < 0 && cod > 3) throw new Exception("Введен не корректный выбор");
+
+
+                try
+                {
+
+                    if (cod == 1)
+                    {
+                        text = Direct.Code(text);
+                        Console.WriteLine($"Зашифрованный код:\n{text}");
+                    }
+                    else if (cod == 3)
+                    {
+                        Console.WriteLine("----Завершение программы----");
+                        break;
+                    }
+                    else
+                    {
+                        text = Direct.UnCode(text);
+                        Console.WriteLine($"Расшифрованный код:\n{text}");
+                    }
+    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine((ex.Message)+"\nПопробуйте еще раз");
+
+                }
+            }
+        }
+    }
+}
